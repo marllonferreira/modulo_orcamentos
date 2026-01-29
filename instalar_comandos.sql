f8e38cada27ed2138debc21d4288b746cc7cb80b
Marllon Ferreira <marllon-ferreira@hotmail.com>
2026-01-29 20:54:58 -0300

    llimpeza de código-02

--- instalar_comandos.sql
+++
@@ -1,8 +0,0 @@
--- 1. Desativa a verificação de chaves estrangeiras
-SET FOREIGN_KEY_CHECKS = 0;
-
--- 2. Limpa a tabela inteira e zera o Auto Incremento
-TRUNCATE TABLE `produtos`;
-
--- 3. Reativa a verificação de chaves estrangeiras
-SET FOREIGN_KEY_CHECKS = 1;
